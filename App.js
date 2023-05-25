bc3357948c7e08cca27344a2314006b486288d9e
valentinArnautski <valentin.arnautski@blankfactor.com>
2023-05-25 10:23:22 +0300

    changes

--- App.js
+++ App.js
@@ -1,23 +1,21 @@
-import { View } from 'react-native';
-import React from 'react';
+import { View } from "react-native";
+import React from "react";
 
-import { NavigationContainer, StackActions } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { LoginScreen } from './screens/LoginScreen';
-import { useNavigation } from '@react-navigation/native';
-import { MenuScreen } from './screens/MenuScreen';
+import { NavigationContainer, StackActions } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { LoginScreen } from "./screens/LoginScreen";
+import { useNavigation } from "@react-navigation/native";
+import { Dashboard } from "./screens/Dashboard";
 
 const Stack = createStackNavigator();
 
 export default function App() {
-
-  return(
+  return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name='Login' component={LoginScreen}/>
-        <Stack.Screen name='Menu' component={MenuScreen}/>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Dashboard" component={Dashboard} />
       </Stack.Navigator>
-
     </NavigationContainer>
-  )
+  );
 }
